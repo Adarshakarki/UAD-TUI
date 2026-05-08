3d3cb7b415b3d404fc533ed9f11e6a5a46e7d4d1
Adarsha Karki <111886610+Adarshakarki@users.noreply.github.com>
2026-05-08 08:17:47 +0545

    add installer

---
+++ install.ps1
@@ -0,0 +1,10 @@
+$installDir = "$env:USERPROFILE\.uad-tui"
+$exePath = "$installDir\uad-tui.exe"
+
+New-Item -ItemType Directory -Force -Path $installDir | Out-Null
+
+Invoke-WebRequest `
+  -Uri "https://github.com/Adarshakarki/UAD-TUI/releases/download/0.1/uad-tui.exe" `
+  -OutFile $exePath
+
+Start-Process $exePath
